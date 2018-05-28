876fd9fe42906159b6f40e80e234f011fe0ae5c1
Robert Han <hanxurobert@gmail.com>
2018-05-27 22:17:10 -0500

    more

--- Tree/Program.cs
+++ Tree/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace Tree
@@ -10,12 +11,20 @@ namespace Tree
             var minHeapTree = new HeapTree();
             var bstTree = new BinarySearchTree();
 
-            List<int> items = new List<int>{12, 22, 33, 45, 67, 231, 90, 0, 77};
+            List<int> items = new List<int>{48, 22, 33, 45, 67, 231, 90, 0, 12, 77};
 
             items.ForEach(item => {
                 minHeapTree.AddNode(item);
                 bstTree.AddNode(item);
             });
+            Console.WriteLine("Print Binary Search Tree (TraversalTree Begin):");
+            var result = TraversalTree(bstTree.Root);
+
+            foreach (var line in result) {
+                Console.WriteLine(string.Join(",", line));
+            }
+
+            Console.WriteLine("Print Binary Search Tree (TraversalTree End).");
 
             Console.WriteLine(string.Join(",", minHeapTree.Items));
 
@@ -43,6 +52,51 @@ namespace Tree
             }
             Console.WriteLine("Print Trie Tree:");
             Console.WriteLine(trieTree.FindCount("robe"));
+        }
+
+        private static List<List<int>> TraversalTree(Node node) {
+            var left = new List<List<int>>();
+            if (node.LeftNode != null){
+                left = TraversalTree(node.LeftNode);
+            }
+            var right = new List<List<int>>();
+            if (node.RightNode != null) {
+                right = TraversalTree(node.RightNode);
+            }
+            
+            return waveLists(left, right, node.Data);
+        }
+
+        private static List<List<int>> waveLists(List<List<int>> first, List<List<int>> second, int root) {
+            var results = new List<List<int>>();
+            if (first.Any() && second.Any()) {
+                foreach(var array1 in first) {
+                    foreach(var array2 in second) {
+                        var result1 = new List<int>();
+                        result1.Add(root);
+                        result1.AddRange(array1);
+                        result1.AddRange(array2);
+                        results.Add(result1);
+                        var result2 = new List<int>();
+                        result2.Add(root);
+                        result2.AddRange(array2);
+                        result2.AddRange(array1);
+                        results.Add(result2);
+                    }
+                }
+            } else if (!first.Any() && !second.Any()) {
+                results.Add(new List<int> { root });
+            } else if (first.Any() || second.Any()) {
+                var arrays = first.Any() ? first : second;
+                foreach (var array in arrays) {
+                    var result1 = new List<int>();
+                    result1.Add(root);
+                    result1.AddRange(array);
+                    results.Add(result1);
+                }
+                    
+            }
+            return results;
         }
     }
 }
